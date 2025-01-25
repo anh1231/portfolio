6d6cef869fc9a04035a8007afd3d49eb41f41fca
Andy Huang <anh031@ucsd.edu>
2025-01-24 19:42:59 -0800

    Update global.js

--- global.js
+++ global.js
@@ -3,3 +3,32 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+ let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname
+  );
+  
+ if (currentLink) {
+    // or if (currentLink !== undefined)
+    currentLink.classList.add('current');
+  }
+
+ let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact'},
+    { url: 'resume/', title: 'Resume'}
+  ];
+
+  let nav = document.createElement('nav');
+  document.body.prepend(nav);
+  
+  for (let p of pages) {
+    const ARE_WE_HOME = document.documentElement.classList.contains('home');
+    let url = p.url;
+    if (!ARE_WE_HOME && !url.startsWith('http')) {
+        url = '../' + url;
+      }
+    let title = p.title;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+  }
+  
