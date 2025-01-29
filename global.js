b9faf83df056dcce96c6e05ff873afd8749f0a8a
Andy Huang <anh031@ucsd.edu>
2025-01-28 23:41:08 -0800

    Update global.js

--- global.js
+++ global.js
@@ -15,10 +15,10 @@ function $$(selector, context = document) {
   }
 
   let pages = [
-    { url: '/', title: 'Home' },
-    { url: 'resume/', title: 'CV' },
-    { url: 'projects/', title: 'Projects' },
-    { url: 'contact/', title: 'Contact' },
+    { url: '../', title: 'Home' },
+    { url: '..resume/', title: 'CV' },
+    { url: '..projects/', title: 'Projects' },
+    { url: '..contact/', title: 'Contact' },
     { url: 'https://github.com/anh1231', title: 'Github' },
   ];
   
